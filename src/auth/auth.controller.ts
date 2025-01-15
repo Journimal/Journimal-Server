03e44d3e0e32a7af4a265e15136c5fdf9a4d8334
dayul <dyulchoo@naver.com>
2025-01-15 12:36:50 +0900

    [feat] lmplement login functionality with user authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { AuthCredentialLoginDto } from './dto/auth-credential-login.dto';
+// import { JwtAuthGuard } from './jwt.guard';
 
 // define the response type
 export interface UserResponse {
@@ -19,4 +21,18 @@ export class AuthController {
   ): Promise<UserResponse> {
     return this.authService.signup(authCredentialDto);
   }
+
+  @Post('log-in')
+  async login(
+    @Body(ValidationPipe) authCredentialLoginDto: AuthCredentialLoginDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.login(authCredentialLoginDto);
+  }
+
+  // if you want to test the jwt guard (when you authentication is required)
+  /* @UseGuards(JwtAuthGuard)
+  @Post('test')
+  test() {
+    return 'success';
+  }*/
 }
